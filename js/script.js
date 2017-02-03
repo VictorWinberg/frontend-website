8a2ab40316fe82a138b61d0625896560830d9e3f
Victor Winberg <dat14vwi@student.lu.se>
2017-02-03 15:57:23 +0100

    Content with float added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 $(document).ready(function() {
-  var unit = ($(window).width() < 768) ? 'mobile' : 'browser';
-  $('section').append('<p class="screen">Your screen width is: ' + $(window).width() + 'px. I think that you are a ' + unit + '!</p>');
-  $('section').append('<p class="mouse"></p>');
-
-  $(window).resize(function() {
-    unit = ($(window).width() < 768) ? 'mobile' : 'browser';
-    $('section p.screen').text('Your screen width is: ' + $(window).width() + 'px. Now I actually think you are a ' + unit + '! Stop changing! It makes me confused!');
-  });
-
-  $(document).on('mousemove', function(event) {
-    $('section p.mouse').text("Your mouse i at x = " + event.pageX + " and y = " + event.pageY + ", isn't that just awesome!?" );
-  });
+  $(document).on('mousemove', function(event) {devMode(event)});
 
   mobileNavbar();
 
@@ -18,24 +7,21 @@ $(document).ready(function() {
 });
 
 function mobileNavbar () {
+  var aboveHeight = $('header').outerHeight();
+
+  $(window).scroll(function() { changeNavbar(); });
   $('.menu-icon').on('click', function(){
     $('.menu-icon').toggleClass('change');
     $('nav ul').toggleClass('is-visible');
     $('nav ul').children().one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() { changeNavbar(); });
   });
 
-  var aboveHeight = $('header').outerHeight();
-  var navHeight = $('nav').outerHeight();
-
-  $(window).scroll(function() { changeNavbar(); });
-
   function changeNavbar() {
     if($(window).width() < 768) {
       if($(window).scrollTop() > aboveHeight) {
-        navHeight = $('nav').outerHeight();
         $('nav').addClass('fixed');
         $('header').css('display', 'none');
-        $('html').css('padding-top', navHeight + aboveHeight + 'px');
+        $('html').css('padding-top', $('nav').outerHeight() + aboveHeight + 'px');
       }
       else {
         $('nav').removeClass('fixed');
@@ -54,4 +40,8 @@ function footerMarginFix() {
     $('footer').css('margin-top', margin + 'px');
   }
   $('footer').css('display', 'block').hide().fadeIn(500);
+}
+
+function devMode(event) {
+  $('footer').html("<b>Footer.</b> Mouse pos: x = " + event.pageX + ", y = " + event.pageY + ". Screen: width = " + $(window).width() + ", height = " + $(window).height() + ". Copyright (c) 2016 Copyright Holder All Rights Reserved.");
 }
