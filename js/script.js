f22ecf82abd3a526ce231e46aadf0328ecc69b68
VictorWinberg <dat14vwi@student.lu.se>
2017-02-05 18:38:28 +0100

    smoothAnchorScroll fixed

--- js/script.js
+++ js/script.js
@@ -18,23 +18,49 @@ function parallaxImages() {
 }
 
 function smoothAnchorScroll() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-     var target = $(this.hash);
-     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-     if (target.length) {
-       $('html, body').animate({
-         scrollTop: target.offset().top
-       }, 1000);
-       return false;
-     }
-   }
- });
+  var header = $('header').outerHeight();
+  var nav = $('nav').outerHeight();
+
+  $('a[href*="#"]').on('click', scrollAnimation);
+  $(window).resize(function() {
+    $('a[href*="#"]').off('click', scrollAnimation);
+    $('a[href*="#"]').on('click', scrollAnimation);
+  });
+
+  function scrollAnimation(e) {
+    //prevent the default "hard" jump
+    e.preventDefault();
+
+    var scrollPos = $(window).scrollTop();
+    var target = $(this).attr("href");
+    var offset = target == '#' ? -scrollPos : $('a[id*=' + target.replace('#','') + ']').offset().top;
+
+    if($(window).width() < 768) {
+      if($(this).closest("nav").length) {
+        offset -= 2 * header + nav;
+      } else {
+        offset -= nav;
+      }
+    }
+
+    if(offset != 0) {
+      // perform animated scrolling by getting top-position of target-element and set it as scroll target
+      $('html, body').animate({
+        scrollTop: scrollPos + offset },
+        1000, function() {
+          //attach the #jumptarget to the pageurl
+          location.hash = target;
+      });
+      return false;
+    }
+  }
 }
 
 function mobileNavbar () {
   var aboveHeight = $('header').outerHeight();
 
+  changeNavbar();
+  $(window).resize(function() { changeNavbar(); });
   $(window).scroll(function() { changeNavbar(); });
   $('.menu-icon').on('click', function(){
     $('.menu-icon').toggleClass('change');
@@ -44,20 +70,18 @@ function mobileNavbar () {
   $('nav a').on('click', function(){
     $('.menu-icon').removeClass('change');
     $('nav ul').removeClass('is-visible');
+    $('nav ul').children().one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() { changeNavbar(); });
   });
 
   function changeNavbar() {
-    if($(window).width() < 768) {
-      if($(window).scrollTop() > aboveHeight) {
+    if($(window).width() < 768 && $(window).scrollTop() > aboveHeight) {
         $('nav').addClass('fixed');
         $('header').css('display', 'none');
         $('html').css('padding-top', $('nav').outerHeight() + aboveHeight + 'px');
-      }
-      else {
-        $('nav').removeClass('fixed');
-        $('header').css('display', 'block');
-        $('html').css('padding-top', '0px');
-      }
+      } else {
+      $('nav').removeClass('fixed');
+      $('header').css('display', 'block');
+      $('html').css('padding-top', '0px');
     }
   }
 }
