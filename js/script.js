cc0bf80e4a1250998084501f30cd47d7cab832f9
VictorWinberg <dat14vwi@student.lu.se>
2017-02-18 16:27:42 +0100

    parallax effect added

--- js/script.js
+++ js/script.js
@@ -11,9 +11,33 @@ $(document).ready(function() {
   });
 });
 
+loadScript("js/parallax.js", function() {
+  // Done loading
+});
+
 function parallaxImages() {
   $('.parallax').each(function(i) {
-    $(this).css('background-image', 'url("img/pattern' + (i+1) + '.jpg")');
+    if($(this).hasClass('layers')) {
+      if($(window).width() < 768) {
+        var div = this;
+        var url = "img/parallax/parallax-" + (i+1) + ".png";
+        var img = new Image();
+        img.onload = function() {
+          // (img-height / img-width * container-width)
+          var height = this.height / this.width * $(window).width();
+          $(div).css('padding-top', height + 'px');
+          $(div).css('background-image', 'url("' + url + '")');
+        }
+        img.src = url;
+      } else {
+        $(this).css('height', $(window).height() - $('.upper-container').outerHeight() + 'px');
+        $(this).children('.layer').each(function(j) {
+          $(this).css('background-image', 'url("img/parallax/parallax-' + (i+1) + '/' + j + '.png")');
+        });
+      }
+    } else {
+      $(this).css('background-image', 'url("img/parallax/parallax-' + (i+1) + '.png")');
+    }
   });
 }
 
@@ -93,4 +117,20 @@ function debug() {
   function mouseAndScreenInfo(event) {
     $('footer').html("<b>Footer.</b> Mouse pos: x = " + event.pageX + ", y = " + event.pageY + ". Screen: width = " + $(window).width() + ", height = " + $(window).height() + ".<br>\&copy; " + new Date().getFullYear() + " Victor Winberg. All Rights Reserved.");
   }
+}
+
+function loadScript(url, callback) {
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
 }
